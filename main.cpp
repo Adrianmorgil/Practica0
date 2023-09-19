3a1b4ef3b14c76cc65ed6de43f5a355a14a8cd94
Adrian Moreno Gil <amorgil@upv.edu.es>
2023-09-19 20:53:57 +0200

    subido el main del brazo

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "BrazoRobotico.h"
+
+int main(){
+	BrazoRobotico brazo(12.5, 20.6, 31.12, false);
+	brazo.mover(6.15, 5.2, 16);
+	std::cout << "El brazo se ha movido correctamente"<< std::endl;
+	brazo.coger();
+	std::cout << "Se cogio el objeto correctamente" << std::endl; 
+}
