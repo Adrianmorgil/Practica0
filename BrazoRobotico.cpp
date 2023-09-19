a91d863938a948a5088bc350b21d974a49db83ee
Adrian Moreno Gil <amorgil@upv.edu.es>
2023-09-19 20:40:11 +0200

    subidos los .cpp y .h del proyecto

--- BrazoRobotico.cpp
+++ BrazoRobotico.cpp
@@ -1,26 +1,40 @@
-#include <BrazoRobotico.h>
+#include "BrazoRobotico.h"
 
-BrazoRobotico(double x_, double y_, double z_, bool objeto_){
+BrazoRobotico::BrazoRobotico(double x_, double y_, double z_, bool objeto_){
 		double x = x_;
 		double y = y_;
 		double z = z_;
-		bool objeto_;}
+		bool objeto_;
+}
 
-double getX(){
-		return x;}
-double getY(){
-		return y;}
-double getZ (){
-		return z;}
-bool getObjeto(){
-		return object;}
+double BrazoRobotico::getX(){
+		return x;
+}
 
-void coger(bool objeto){
-		objeto == true;}
-void soltar(bool objeto){
-		objeto == false;}
-void mover(double x, double y, double z){
+double BrazoRobotico::getY(){
+		return y;
+}
+
+double BrazoRobotico::getZ (){
+		return z;
+}
+
+bool BrazoRobotico::getObjeto(){
+		return object;
+}
+
+
+void BrazoRobotico::coger(bool objeto){
+		objeto = true;
+}
+
+void BrazoRobotico::soltar(bool objeto){
+		objeto = false;
+}
+
+void BrazoRobotico::mover(double x, double y, double z){
 		x = newx;
 		y = newy;
-		z = newz;}
+		z = newz;
+}
 }
