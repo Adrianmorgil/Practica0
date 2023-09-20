5cdac8e86e1d823d9333a029841f7f708ed19a48
Adrian Moreno Gil <amorgil@upv.edu.es>
2023-09-20 17:04:42 +0200

    main subido con las posiciones

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(){
 	BrazoRobotico brazo(12.5, 20.6, 31.12, false);
 	brazo.mover(6.15, 5.2, 16);
-	std::cout << "El brazo se ha movido correctamente"<< std::endl;
+	std::cout << "El brazo se ha movido correctamente" << std::endl;
 	brazo.coger();
 	std::cout << "Se cogio el objeto correctamente" << std::endl; 
 }
